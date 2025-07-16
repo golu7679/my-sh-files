ab781069d3ccb78293151cb1a76a1aa002ffeff7
golu <55990159+golu7679@users.noreply.github.com>
2025-07-16 19:58:39 +0530

    multiple device select added on adb_reverse.sh

--- adb_reverse.sh
+++ adb_reverse.sh
@@ -14,31 +14,38 @@ fi
 device_options=()
 device_serials=()
 
+index=1
 for serial in $device_list; do
   model=$(adb -s "$serial" shell getprop ro.product.model | tr -d '\r')
   name=$(adb -s "$serial" shell getprop ro.product.name | tr -d '\r')
   manufacturer=$(adb -s "$serial" shell getprop ro.product.manufacturer | tr -d '\r')
-  device_options+=("$manufacturer $model ($name) - [$serial]")
+  device_string="$manufacturer $model ($name) - [$serial]"
+  device_options+=("$device_string")
   device_serials+=("$serial")
+  echo "$index) $device_string"
+  ((index++))
 done
 
-# Display selection menu
+# Ask for multiple device selection
 echo ""
-echo "Select a device to reverse ports:"
-select choice in "${device_options[@]}"; do
-  if [[ -n "$choice" ]]; then
-    index=$((REPLY - 1))
+echo "Enter the numbers of the devices to reverse ports (e.g. 1 2 3):"
+read -r -a selections
+
+for num in "${selections[@]}"; do
+  if [[ "$num" =~ ^[0-9]+$ ]] && (( num >= 1 && num <= ${#device_serials[@]} )); then
+    index=$((num - 1))
     selected_serial="${device_serials[$index]}"
+    selected_device="${device_options[$index]}"
+
     echo ""
-    echo "You selected: $choice"
+    echo "You selected: $selected_device"
     echo "Reversing ports on $selected_serial..."
 
     adb -s "$selected_serial" reverse tcp:8000 tcp:8000
     adb -s "$selected_serial" reverse tcp:8081 tcp:8081
 
     echo "Reverse TCP done for $selected_serial"
-    break
   else
-    echo "Invalid selection. Try again."
+    echo "Invalid selection: $num"
   fi
 done
